d150c59856d76ac027ecb934da4371ab2f626e8a
Pratik Zimbre <zimbrepratik@gmail.com>
2025-01-03 23:48:12 +0530

    BSN-0001

--- src/main/java/com/personal/book/BookNetworkApiApplication.java
+++ src/main/java/com/personal/book/BookNetworkApiApplication.java
@@ -4,7 +4,7 @@ package com.personal.book;
  * #%L
  * book-network-api
  * %%
- * Copyright (C) 2024 Book Social Network
+ * Copyright (C) 2024 - 2025 Book Social Network
  * %%
  * Book Social Network, Inc. and / or its subsidiaries - All Rights Reserved worldwide. 
  * This document is protected under the trade secret and copyright laws as the property of Book Social Network, Inc. and / or its subsidiaries.
@@ -12,6 +12,7 @@ package com.personal.book;
  * Any disclosure of this document to third parties is strictly prohibited.
  * #L%
  */
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
